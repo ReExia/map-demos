0fb2d6c3addb1816e764c72dacd32fc4e15bc81f
setsunahao <liu1023434681@qq.com>
2018-09-03 20:43:56 +0800

    en....

---
+++ gaode/src/main/java/com/example/gaode/GaodeApplication.java
@@ -0,0 +1,12 @@
+package com.example.gaode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaodeApplication.class, args);
+    }
+}
---
+++ gaode/src/main/resources/static/README.md
@@ -0,0 +1,4 @@
+高德地图
+
+key 
+ <script type="text/javascript" src="https://webapi.amap.com/maps?v=1.3&key=c75741b6f2144f1c55dddbb864d76b4b"></script>
